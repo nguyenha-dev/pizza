8d0c8bad5e560bb1d5c03a6ceedf88ff758d682b
nguyen.hm <nguyen.hm@neo-lab.vn>
2021-06-17 14:43:36 +0700

    Init skeleton for project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import './styles/index.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Navbar = lazy(() => import('./components/Navbar'));
+const Footer = lazy(() => import('./components/Footer'));
+const Home = lazy(() => import('./pages/Home'));
+const Menu = lazy(() => import('./pages/Menu'));
+const About = lazy(() => import('./pages/About'));
+const Contact = lazy(() => import('./pages/Contact'));
+
+const App = () => {
+    return (
+        <div className="App">
+            <Suspense fallback={<div>Loading...</div>}>
+                <Router>
+                    <Navbar/>
+                    <Switch>
+                        <Route exact path='/' component={Home}/>
+                        <Route exact path='/menu' component={Menu}/>
+                        <Route exact path='/about' component={About}/>
+                        <Route exact path='/contact' component={Contact}/>
+                        <Redirect to='/'/>
+                    </Switch>
+                    <Footer/>
+                </Router>
+            </Suspense>
+        </div>
+    );
 }
 
 export default App;
